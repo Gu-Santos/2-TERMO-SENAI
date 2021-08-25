76c710c6636cb127d43f38d043bf59f2ae028e73
Gu-Santos <tjgborges12@gmail.com>
2021-08-25 10:17:28 -0300

    inicio web_API_filmes

---
+++ sprint-2-be/senai_filmes_webAPI/senai_filmes_webAPI/Domains/FilmeDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    public class FilmeDomain
+    {
+        public int IDfilme { get; set; }
+        public int IDgenero { get; set; }
+        public string Titulo_filme { get; set; }
+    }
+}
